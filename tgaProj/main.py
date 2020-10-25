3f79da9e120ec395b48e74b0ad1b1fde450f2929
Roy canani <roy33441c@gmail.com>
2020-10-25 19:30:13 +0200

    added feat to main

--- tgaProj/main.py
+++ tgaProj/main.py
@@ -14,9 +14,11 @@ def stam():
 
 @app.route('/teams', methods=['GET','POST'])
 def teams_by_students_route():
-    students_sended = request.json
+    request_body = request.json
     students: List[Student] = []
+    number_of_teams: int = request_body["team_length"]
     for student in students_sended["students"]:
         students.append(Student(student["id"], student["grade"], student["eval_grade"]))
     print(students)
+
     return "Thank you"
